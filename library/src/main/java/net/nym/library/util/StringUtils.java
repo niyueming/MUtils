16b3fb12d74390533984dfebfffb702e90ea7287
niyueming <niyueming@163.com>
2014-10-31 11:02:58 +0800

    半角转全角

--- library/src/main/java/net/nym/library/util/StringUtils.java
+++ library/src/main/java/net/nym/library/util/StringUtils.java
@@ -69,6 +69,25 @@ public class StringUtils {
     }
 
     /**
+     * 半角转换为全角
+     *
+     * @param input
+     * @return
+     */
+    public static String ToDBC(String input) {
+        char[] c = input.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] == 12288) {
+                c[i] = (char) 32;
+                continue;
+            }
+            if (c[i] > 65280 && c[i] < 65375)
+                c[i] = (char) (c[i] - 65248);
+        }
+        return new String(c);
+    }
+
+    /**
      *
      * @param time 毫秒时间差
      * @return 返回00:00:00 格式
